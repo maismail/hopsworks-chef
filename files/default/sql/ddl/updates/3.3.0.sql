2cd3348924b286ad2b3aa1a945c40f0d5be88c37
Mahmoud Ismail <mahmoud@hopsworks.ai>
2023-07-13 11:40:29 +0200

    fixes for review

--- files/default/sql/ddl/updates/3.3.0.sql
+++ files/default/sql/ddl/updates/3.3.0.sql
@@ -240,5 +240,7 @@ ALTER TABLE `hopsworks`.`feature_store_statistic`
 ALTER TABLE `hopsworks`.`git_executions` ADD COLUMN `hostname` VARCHAR(128) COLLATE latin1_general_cs NOT NULL DEFAULT "localhost";
 
 -- FSTORE-857
+SET SQL_SAFE_UPDATES = 0;
 UPDATE hopsworks.jobs SET json_config=JSON_SET(json_config, '$.appPath', 'hdfs:///user/spark/hsfs-utils-3.3.0-RC0.jar') WHERE JSON_EXTRACT(json_config, '$.appPath') like '"hdfs:///user/spark/hsfs-utils-%.jar"';
 UPDATE hopsworks.jobs SET json_config=JSON_SET(json_config, '$.appPath', 'hdfs:///user/spark/hsfs_utils-3.3.0-RC0.py') WHERE JSON_EXTRACT(json_config, '$.appPath') like '"hdfs:///user/spark/hsfs_utils-%.py"';
+SET SQL_SAFE_UPDATES = 1;
